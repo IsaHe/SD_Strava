c9e89a8399f04e53eecb0547f6cea653ea9067b2
isaacherbozo <isaacherbozo06@gmail.com>
2024-10-31 12:36:55 +0100

    Initial commit

---
+++ src/main/java/es/deusto/sd/strava/sd_strava/SdStravaApplication.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.strava.sd_strava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdStravaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdStravaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/deusto/sd/strava/sd_strava/SdStravaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.strava.sd_strava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdStravaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
